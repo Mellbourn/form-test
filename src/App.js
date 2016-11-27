0f97041c1e6aaa35adb29ee7d134f146af7cf84b
Klas Mellbourn <klas@mellbourn.net>
2016-11-27 17:17:11 +0100

    intial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const NewInput = (() => {
+  return <input type="text" placeholder="writeme" />
+})
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React, Klas</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <NewInput />
+      </div>
+    );
+  }
+}
+
+export default App;
